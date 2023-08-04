1faee4a9aaf90e0e7f2cb5ea5d28cdedd1545f2c
JesusMena <jesusalbertomena2002@gmail.com>
2023-08-04 15:14:47 -0600

    use client removed

--- src/app/panel-admin/layout.tsx
+++ src/app/panel-admin/layout.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Providers from "./Providers";
 import AdminHeader from "@/components/Admin/Header/AdminHeader";
