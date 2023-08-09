0df8399e19935d7e8fc48418eb395cdbdf07fd5c
JesusMena <jesusalbertomena2002@gmail.com>
2023-08-09 11:13:20 -0600

    Init Modified

--- src/app/api/init/route.ts
+++ src/app/api/init/route.ts
@@ -4,55 +4,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const encryptedPassword = await encryptPassword( process.env.ADMIN_USER_PASSWORD ?? "adminPassword123");
-
-    const defaultAdmin = await prisma.user.upsert({
-      where: {
-        userID: 1,
-      },
-      update: {
-        password: encryptedPassword,
-      },
-      create: {
-        email:  process.env.ADMIN_USER_EMAIL ?? "defaultadmin@casanj.com",
-        password: encryptedPassword,
-        userName: "DefaultAdmin",
-      },
-    });
-
-    const childrenGallery = await prisma.gallery.upsert({
-      where: { galleryID: 1 },
-      update: { name: "Niños" },
-      create: { name: "Niños" },
-    });
-
-    const menGallery = await prisma.gallery.upsert({
-      where: { galleryID: 2 },
-      update: { name: "Hombres" },
-      create: { name: "Hombres" },
-    });
-
-    const womenGallery = await prisma.gallery.upsert({
-      where: { galleryID: 3 },
-      update: { name: "Mujeres" },
-      create: { name: "Mujeres" },
-    });
-
-    const specialGallery = await prisma.gallery.upsert({
-      where: { galleryID: 4 },
-      update: { name: "Especiales" },
-      create: { name: "Especiales" },
-    });
-
-    const defaultPsalm = await prisma.weekPsalm.upsert({
-      where: { PsalmID: 1 },
-      update: {
-        content: "<p>Salmo semanal</p>",
-      },
-      create: {
-        content: "<p>Salmo semanal</p>",
-      },
-    });
+    await checUser()
+    await checFirstGallery()
+    await checSecondGallery()
+    await checThirdGallery()
+    await checFourtGallery()
+    await checPsalm()
 
     return NextResponse.json({ message: "Data is al ready" }, { status: 200 });
   } catch (error) {
@@ -66,3 +23,94 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+const checUser = async () => {
+  const encryptedPassword = await encryptPassword(
+    process.env.ADMIN_USER_PASSWORD ?? "adminPassword123"
+  );
+
+  const user = await prisma.user.findUnique({
+    where: {
+      userID: 1,
+    },
+  });
+
+  if (user) {
+    throw new Error("Ya existe el usuario");
+  }
+
+  const defaultAdmin = await prisma.user.create({
+    data: {
+      email: process.env.ADMIN_USER_EMAIL ?? "defaultadmin@casanj.com",
+      password: encryptedPassword,
+      userName: "DefaultAdmin",
+    },
+  });
+};
+const checFirstGallery = async () => {
+  const gallery = await prisma.gallery.findUnique({
+    where: { galleryID: 1 },
+  });
+
+  if (gallery) {
+    throw new Error("Ya existe la galería");
+  }
+
+  const childrenGallery = await prisma.gallery.create({
+    data: { name: "Niños" },
+  });
+};
+const checSecondGallery = async () => {
+  const gallery = await prisma.gallery.findUnique({
+    where: { galleryID: 2 },
+  });
+
+  if (gallery) {
+    throw new Error("Ya existe la galería");
+  }
+
+  const menGallery = await prisma.gallery.create({
+    data: { name: "Hombres" },
+  });
+};
+const checThirdGallery = async () => {
+  const gallery = await prisma.gallery.findUnique({
+    where: { galleryID: 3 },
+  });
+
+  if (gallery) {
+    throw new Error("Ya existe la galería");
+  }
+
+  const womenGallery = await prisma.gallery.create({
+    data: { name: "Mujeres" },
+  });
+};
+const checFourtGallery = async () => {
+  const gallery = await prisma.gallery.findUnique({
+    where: { galleryID: 4 },
+  });
+
+  if (gallery) {
+    throw new Error("Ya existe la galería");
+  }
+
+  const specialGallery = await prisma.gallery.create({
+    data: { name: "Especiales" },
+  });
+};
+const checPsalm = async () => {
+  const psalm = await prisma.weekPsalm.findUnique({
+    where: { PsalmID: 1 },
+  });
+
+  if (psalm) {
+    throw new Error("Ya existe el salmo");
+  }
+
+  const defaultPsalm = await prisma.weekPsalm.create({
+    data: {
+      content: "<p>Salmo semanal</p>",
+    },
+  });
+};
