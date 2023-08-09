e9f808db676635b6589ca41d7493a2f5b4b67114
JesusMena <jesusalbertomena2002@gmail.com>
2023-08-09 11:52:03 -0600

    Prisma modified

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,31 +5,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma =
-  globalForPrisma.prisma?.$extends({
-    result: {
-      image: {
-        url: {
-          needs: { imageURL: true },
-          compute(image) {
-            return `localhost:3000/galleries/${image.imageURL}`;
-          },
-        },
-      },
-    },
-  }) ??
-  new PrismaClient().$extends({
-    result: {
-      image: {
-        url: {
-          needs: { imageURL: true },
-          compute(image) {
-            return `localhost:3000/galleries/${image.imageURL}`;
-          },
-        },
-      },
-    },
-  });
+const prisma = globalForPrisma.prisma;
 
 async function createData() {
   try {
